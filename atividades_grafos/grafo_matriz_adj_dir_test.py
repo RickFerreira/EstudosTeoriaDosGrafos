6c1655bc52039c8f69f24170b4b828388d74df4e
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-04-24 00:11:23 -0300

    Arquivos uteis

---
+++ atividades_grafos/grafo_matriz_adj_dir_test.py
@@ -0,0 +1,120 @@
+import unittest
+from meu_grafo_matriz_adj_dir import *
+from bibgrafo.grafo_exceptions import *
+
+class TestGrafo(unittest.TestCase):
+
+    def setUp(self):
+        # Grafo da Paraíba
+        self.g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p.adicionaAresta('a1', 'J', 'C')
+        self.g_p.adicionaAresta('a2', 'C', 'E')
+        self.g_p.adicionaAresta('a3', 'C', 'E')
+        self.g_p.adicionaAresta('a4', 'P', 'C')
+        self.g_p.adicionaAresta('a5', 'P', 'C')
+        self.g_p.adicionaAresta('a6', 'T', 'C')
+        self.g_p.adicionaAresta('a7', 'M', 'C')
+        self.g_p.adicionaAresta('a8', 'M', 'T')
+        self.g_p.adicionaAresta('a9', 'T', 'Z')
+
+        # Grafo da Paraíba sem arestas paralelas
+        self.g_p_sem_paralelas = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_sem_paralelas.adicionaAresta('a1', 'J', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a2', 'C', 'E')
+        self.g_p_sem_paralelas.adicionaAresta('a3', 'P', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a4', 'T', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a5', 'M', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a6', 'M', 'T')
+        self.g_p_sem_paralelas.adicionaAresta('a7', 'T', 'Z')
+
+        # Grafos completos
+        self.g_c = MeuGrafo(['J', 'C', 'E', 'P'])
+        self.g_c.adicionaAresta('a1','J','C')
+        self.g_c.adicionaAresta('a2', 'J', 'E')
+        self.g_c.adicionaAresta('a3', 'J', 'P')
+        self.g_c.adicionaAresta('a4', 'E', 'C')
+        self.g_c.adicionaAresta('a5', 'P', 'C')
+        self.g_c.adicionaAresta('a6', 'P', 'E')
+
+        self.g_c2 = MeuGrafo(['Nina', 'Maria'])
+        self.g_c2.adicionaAresta('amiga', 'Nina', 'Maria')
+
+        self.g_c3 = MeuGrafo(['J'])
+
+        # Grafos com laco
+        self.g_l1 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l1.adicionaAresta('a1', 'A', 'A')
+        self.g_l1.adicionaAresta('a2', 'A', 'B')
+        self.g_l1.adicionaAresta('a3', 'A', 'A')
+
+        self.g_l2 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l2.adicionaAresta('a1', 'A', 'B')
+        self.g_l2.adicionaAresta('a2', 'B', 'B')
+        self.g_l2.adicionaAresta('a3', 'B', 'A')
+
+        self.g_l3 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l3.adicionaAresta('a1', 'C', 'A')
+        self.g_l3.adicionaAresta('a2', 'C', 'C')
+        self.g_l3.adicionaAresta('a3', 'D', 'D')
+        self.g_l3.adicionaAresta('a4', 'D', 'D')
+
+        self.g_l4 = MeuGrafo(['D'])
+        self.g_l4.adicionaAresta('a1', 'D', 'D')
+
+        self.g_l5 = MeuGrafo(['C', 'D'])
+        self.g_l5.adicionaAresta('a1', 'D', 'C')
+        self.g_l5.adicionaAresta('a2', 'C', 'C')
+
+        # Grafos desconexos
+        self.g_d = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_d.adicionaAresta('asd', 'A', 'B')
+
+        # Grafo com ciclos e laços
+        self.g_e = MeuGrafo(['A', 'B', 'C', 'D', 'E'])
+        self.g_e.adicionaAresta('1', 'A', 'B')
+        self.g_e.adicionaAresta('2', 'A', 'C')
+        self.g_e.adicionaAresta('3', 'C', 'A')
+        self.g_e.adicionaAresta('4', 'C', 'B')
+        self.g_e.adicionaAresta('10', 'C', 'B')
+        self.g_e.adicionaAresta('5', 'C', 'D')
+        self.g_e.adicionaAresta('6', 'D', 'D')
+        self.g_e.adicionaAresta('7', 'D', 'B')
+        self.g_e.adicionaAresta('8', 'D', 'E')
+        self.g_e.adicionaAresta('9', 'E', 'A')
+        self.g_e.adicionaAresta('11', 'E', 'B')
+
+        # Matrizes para teste do algoritmo de Warshall
+
+        self.g_p_m = self.constroi_matriz(self.g_p)
+        self.g_p_m[0][1] = 1
+        self.g_p_m[0][2] = 1
+        self.g_p_m[1][2] = 1
+        self.g_p_m[3][1] = 1
+        self.g_p_m[3][2] = 1
+        self.g_p_m[4][1] = 1
+        self.g_p_m[4][2] = 1
+        self.g_p_m[4][5] = 1
+        self.g_p_m[4][6] = 1
+        self.g_p_m[5][1] = 1
+        self.g_p_m[5][2] = 1
+        self.g_p_m[5][6] = 1
+
+        self.g_e_m = self.constroi_matriz(self.g_e)
+        for i in range(0, len(self.g_e_m)):
+            self.g_e_m[0][i] = 1
+            self.g_e_m[2][i] = 1
+            self.g_e_m[3][i] = 1
+            self.g_e_m[4][i] = 1
+
+    def constroi_matriz(self, g: MeuGrafo):
+        ordem = len(g.N)
+        m = list()
+        for i in range(ordem):
+            m.append(list())
+            for j in range(ordem):
+                m[i].append(0)
+        return m
+
+    def test_warshall(self):
+        self.assertEqual(self.g_p.warshall(), self.g_p_m)
+        self.assertEqual(self.g_e.warshall(), self.g_e_m)
