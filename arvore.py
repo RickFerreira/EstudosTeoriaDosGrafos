6c18f2f923b1074506ec1fed16f1be1731dfe62c
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-04-24 12:36:35 -0300

    Delete arvore.py

--- arvore.py
+++
@@ -1,9 +0,0 @@
-#Primeiro grafo utilizando a biblioteca igrafh e a dependencia cairo
-from igraph import *
-
-#gerando um grafo de arvore com 31 vertices
-#cada vertice tem dois filhos e um pai até completar os 31
-grafoArvore = Graph.Tree(31, 2)
-
-plot(grafoArvore)
-
