a0b3a83db77115080933fd1281d39f142967f3c1
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-05-08 14:43:07 -0300

    atividade Roteiro 1 terminada

--- atividades_grafos/meu_grafo.py
+++ atividades_grafos/meu_grafo.py
@@ -3,64 +3,45 @@ from bibgrafo.grafo_exceptions import *
 
 
 class MeuGrafo(GrafoListaAdjacencia):
-#
+
     def vertices_nao_adjacentes(self):
         '''
-        Provê uma lista de vértices não adjacentes no grafo. O conjunto terá o seguinte formato: [X-Z, X-W, ...]
+        Provê uma lista de vértices não adjacentes no grafo. A lista terá o seguinte formato: [X-Z, X-W, ...]
         Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
         :return: Uma lista com os pares de vértices não adjacentes
         '''
-        #nos primeiros for ele coloca um indice pra comparar 
         vna = set()
-        
-        for v1 in self.N:
-            for v2 in self.N:
-                #verifica item por item comparado com outros itens e ve se existe ele na lista de arestas, assim irá saber se tem alguma aresta com aqueles indices
-                achei = False
-                for a in self.A:
-                    if (v1 != v2):
-                        if ((v1 == self.A[a].getV1() and v2 == self.A.[a].getV2()) or (v2 == self.A[a].getV1() and v1 == self.A.[a].getV2()):
-                            (v2 == self.A[a].getV1() and V1 == self.A[a].getV2()):
-                            achei = True
-                if not achei and v1 != v2:           
-                    vna.add("{}-{}").format((v1, v2)
-                                            
-        return vna
-        
-        
-        '''vertices_nao_adjacentes = []
-
+        #percorrer a lista de vertices
         for i in self.N:
-            vertices_adjacentes = []
-            for j in self.A:
-                v1 = self.A[j].getV1()
-                v2 = self.A[j].getV2()
-                if v1 == i:
-                    vertices_adjacentes.append(v2)
-                elif v2 == i:
-                    vertices_adjacentes.append(v1)
+            for j in self.N:
+                achei = False
+                #percorre a lista de arestas
+                for k in self.A:
+                    if i != j:
+                        #se naquela aresta o i e o j atual forem encontrados idependente da ordem,
+                        #quer dizer que naquela configuração existe aresta, ou seja i e j são adjacente
+                        if (i == self.A[k].getV1() and j == self.A[k].getV2()) or \
+                                (j == self.A[k].getV1() and i == self.A[k].getV2()):
+                            achei = True
+                #cria uma dupla de arestas que não tem ligação
+                if not achei and i != j:
+                    vna.add("{}-{}".format(i, j))
+        #retorna a lista criada com todos os itens não adjacentes
+        return vna
 
-            for k in self.N:
-                if k != i and k not in vertices_adjacentes:
-                    a_test_1 = f'{k}-{k}'
-                    a_test_2 = f'{k}-{i}'
-                    if a_test_1 not in vertices_nao_adjacentes and a_test_2 not in vertices_nao_adjacentes:
-                        vertices_nao_adjacentes.append(f'{i}-{k}')
-
-            vertices_adjacentes = []
-
-        return vertices_nao_adjacentes
-        '''
     def ha_laco(self):
         '''
         Verifica se existe algum laço no grafo.
         :return: Um valor booleano que indica se existe algum laço.
         '''
-        for a in self.A:
-            if self.A[a].getV1() == self.A[a].getV2():
-                return True
-
-        return False
+        saida = False
+        #percorre a lista de arestas
+        for i in self.A:
+            #verifica se o v1 é igual ao v2 naquela aresta
+            if self.A[i].getV1() == self.A[i].getV2():
+                #caso for troca a saida para verdadeiro
+                saida = True
+        return saida
 
     def grau(self, V=''):
         '''
@@ -69,51 +50,45 @@ class MeuGrafo(GrafoListaAdjacencia):
         :return: Um valor inteiro que indica o grau do vértice
         :raises: VerticeInvalidoException se o vértice não existe no grafo
         '''
-        vertices_adjacentes = []
-
-        verticeExiste = False
-
-        for vt in self.N:
-            if vt == V:
-                verticeExiste = True
-                vertices_adjacentes = []
-                for a in self.A:
-                    v1 = self.A[a].getV1()
-                    v2 = self.A[a].getV2()
-                    if v1 == vt:
-                        vertices_adjacentes.append(v2)
-                    if v2 == vt:
-                        vertices_adjacentes.append(v1)
-
-        if len(vertices_adjacentes) == 0 and verticeExiste == False:
-            raise VerticeInvalidoException('O vértice ' + V + ' não existe no grafo')
+        achouOVertice = False
+        numeroDeGraus = 0
+        for i in self.N:
+            #percorrendo toda a lista de vertices para procurar o V
+            if i == V:
+                #achou o V e agora vai olhar quantas arestas tem
+                achouOVertice = True
+                #percorre alista de arestas e verifica se o v1 ou o v2 de cada aresta é igual ao vertice achado
+                for j in self.A:
+                    if self.A[j].getV1() == i:
+                        numeroDeGraus += 1
+                    if self.A[j].getV2() == i:
+                        numeroDeGraus += 1
+                #a quantidade de vezes que aparece o vertice nas arestas é o grau dele
+        #se não achar o vertice pedido da o erro
+        if not achouOVertice:
+            raise VerticeInvalidoException()
         else:
-            return len(vertices_adjacentes)
+            return numeroDeGraus
 
     def ha_paralelas(self):
         '''
         Verifica se há arestas paralelas no grafo
         :return: Um valor booleano que indica se existem arestas paralelas no grafo.
         '''
-        for a1 in self.A:
-            v1_a1 = self.A[a1].getV1()
-            v2_a1 = self.A[a1].getV2()
-
-            a1_str = f'{v1_a1}-{v2_a1}'
-            a1_str_swap = f'{v2_a1}-{v1_a1}'
-
-            for a2 in self.A:
-                if a1 != a2:
-                    v1_a2 = self.A[a2].getV1()
-                    v2_a2 = self.A[a2].getV2()
-                    a2_str = f'{v1_a2}-{v2_a2}'
-                    a2_str_swap = f'{v2_a2}-{v1_a2}'
-
-                    if a1_str == a2_str or a1_str == a2_str_swap or a2_str == a1_str_swap:
-                        return True
-
-        return False
-
+        saida = False
+        #percorrendo a lista de arestas
+        for i in self.A:
+            cont = 0
+            #procurando se existem arestas iguais ou as mesmas só trocando a ordem
+            for j in self.A:
+                if (self.A[i].getV1() == self.A[j].getV1()) and (self.A[i].getV2() == self.A[j].getV2()):
+                    cont += 1
+                elif (self.A[i].getV1() == self.A[j].getV2()) and (self.A[i].getV2() == self.A[j].getV1()):
+                    cont += 1
+            #verificando se achou iguais pelos menos duas vezes para poder ser paralela
+            if cont >= 2:
+                saida = True
+        return saida
 
     def arestas_sobre_vertice(self, V):
         '''
@@ -122,42 +97,51 @@ class MeuGrafo(GrafoListaAdjacencia):
         :return: Uma lista os rótulos das arestas que incidem sobre o vértice
         :raises: VerticeInvalidoException se o vértice não existe no grafo
         '''
-        arestas_adjacentes = []
-
-        verticeExiste = False
-
-        for vt in self.N:
-            if vt == V:
-                verticeExiste = True
-                for a in self.A:
-                    v1 = self.A[a].getV1()
-                    v2 = self.A[a].getV2()
-                    if v1 == vt or v2 == vt:
-                        if a not in arestas_adjacentes:
-                            arestas_adjacentes.append(a)
-
-        if len(arestas_adjacentes) == 0 and verticeExiste == False:
-            raise VerticeInvalidoException('O vértice ' + V + ' não existe no grafo')
+        achouOVertice = False
+        listaArestas = set()
+        for i in self.N:
+            #percorrendo toda a lista de vertices para procurar o V
+            if i == V:
+                achouOVertice = True
+            #achou o V e agora vai percorrer a lista de arestas
+                for j in self.A:
+                    #se na aresta atual o v1 ou o v2 forem igual ao vertice passado adiciona então a aresta na lista
+                    if (self.A[j].getV1() == V) or (self.A[j].getV2() == V):
+                        listaArestas.add("{}".format(j))
+        #se não achar o vertice pedido da o erro
+        if not achouOVertice:
+            raise VerticeInvalidoException()
         else:
-            return arestas_adjacentes
+            #retorna a lista de arestas
+            return listaArestas
 
     def eh_completo(self):
         '''
         Verifica se o grafo é completo.
         :return: Um valor booleano que indica se o grafo é completo
         '''
-        if self.ha_laco() or self.ha_paralelas():
-            return False
-
-        else:
-            totalVertices = len(self.N)
-            for v in self.N:
-                if self.grau((v)) == totalVertices - 1:
-                    continue
-                else:
-                    return False
-
-            return True
+        #nesse caso vou ter que chamar outras funções que já fiz para descobrir se é completo
+        #grafo completo não tem laço, não te paralela e todos os vertices se conectam
+        saida = True
+        #verificando quantas vertices tem
+        numeroDeVertices = len(self.N)
+        #se tiver laço não é completo
+        if self.ha_laco():
+            saida = False
+        #se tiver paralela não é completo
+        elif self.ha_paralelas():
+            saida = False
+        #percorrendo a lista de vertices para verificar se todos são tocados
+        for i in self.N:
+            #verificando quantos graus tem cada vertice de acordo com a função grau
+            grau = self.grau(i)
+            #para cada vertice eu vejo se ele toca na mesma quantidade de vertices total menos ele mesmo
+            if grau < numeroDeVertices-1:
+                #ja sabendo que não tem laço e paralela verifico só se o grau tá correto
+                #se o grau atual for menor do que o número de vertices-1 quer dizer que não seria completo pois faltaria tocar em algum
+                saida = False
+        #se tiver passado por todos sem mudar a saida ela vai ser true
+        return saida
 
     def dijkstra_drone(self, vi, vf, carga: int, carga_max: int, pontos_recarga: list()):
         pass
